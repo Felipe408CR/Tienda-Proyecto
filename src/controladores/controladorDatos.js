169b5b200d062604d4c67cbda7cad138764620e8
NOCTURNO <q.rojas.carlos@gmail.com>
2022-07-11 12:32:16 -0500

    Se crean Controladores para cada función, se confugura footer

---
+++ src/controladores/controladorDatos.js
@@ -0,0 +1,134 @@
+export let productosBD=[
+
+    {
+        nombre:"Bugatti Chiron",
+        precio: 4300000,
+        cantidad:0,
+        fotos:['https://firebasestorage.googleapis.com/v0/b/felipe408crtiendatcc.appspot.com/o/Bugatti_Chiron-%202.webp?alt=media&token=0f66aba4-734c-4246-ac21-e46cf726ac45','https://firebasestorage.googleapis.com/v0/b/felipe408crtiendatcc.appspot.com/o/Bugatti_Chiron-%201.jpg?alt=media&token=aa7090c6-df44-4c45-a934-0958fdb86122'],
+        descripcion:'Super Deportivo de Bugatti',
+    },
+
+    {
+        nombre:'Bugatti La Voiture Noire',
+        precio:16500000,
+        cantidad:0,
+        fotos:['https://firebasestorage.googleapis.com/v0/b/felipe408crtiendatcc.appspot.com/o/bugatti%20la%20voiture%201.jpg?alt=media&token=ece09de9-afdc-4aa0-95c7-87dfb30c6f58','https://firebasestorage.googleapis.com/v0/b/felipe408crtiendatcc.appspot.com/o/bugatti%20la%20voiture%202.jpg?alt=media&token=ac2ec12a-177c-4b5f-9aaf-1502d5ebad11'],
+        descripcion:'Super Deportivo de Bugatti',
+    },
+
+    {
+        nombre:'Bugatti Veyron',
+        precio:1500000,
+        cantidad:0,
+        fotos:['https://firebasestorage.googleapis.com/v0/b/felipe408crtiendatcc.appspot.com/o/bugatti%20veyron%201.jpg?alt=media&token=b511b286-54e0-47db-a408-e534b4a6e9f0','https://firebasestorage.googleapis.com/v0/b/felipe408crtiendatcc.appspot.com/o/bugatti%20veyron%202.jpg?alt=media&token=b4ce805c-1d41-4fb2-abd5-3b86a38362d4'],
+        descripcion:'Super Deportivo de Bugatti',
+    },
+
+    {
+        nombre:'Bugatti Divo',
+        precio:5000000,
+        cantidad:0,
+        fotos:['https://firebasestorage.googleapis.com/v0/b/felipe408crtiendatcc.appspot.com/o/bugatti%20divo%201.jpg?alt=media&token=74f48417-8869-4a4c-8e71-a504b453fb8b','https://firebasestorage.googleapis.com/v0/b/felipe408crtiendatcc.appspot.com/o/bugatti%20divo%202.jpg?alt=media&token=ffb35b9c-d967-4c80-8370-697622226d0c'],
+        descripcion:'Super Deportivo de Bugatti',
+    },
+
+    {
+        nombre:'Bugatti Centodieci',
+        precio:8000000,
+        cantidad:0,
+        fotos:['https://firebasestorage.googleapis.com/v0/b/felipe408crtiendatcc.appspot.com/o/bugatti%20centodieci%201.jfif?alt=media&token=f2466291-5886-4184-89b5-d29b5fda56cb','https://firebasestorage.googleapis.com/v0/b/felipe408crtiendatcc.appspot.com/o/bugatti%20centodieci%202.jpeg?alt=media&token=90720b26-0f3d-4772-9829-8ae596c4a211'],
+        descripcion:'Super Deportivo de Bugatti',
+    },
+
+    {
+        nombre:'Ferrari F8 Spider',
+        precio:262000,
+        cantidad:0,
+        fotos:['https://firebasestorage.googleapis.com/v0/b/felipe408crtiendatcc.appspot.com/o/ferrari%20F8%20sipder.jpg?alt=media&token=d223495b-bd74-475a-bec9-1dbd92a1651e','https://firebasestorage.googleapis.com/v0/b/felipe408crtiendatcc.appspot.com/o/ferrari%20f8%20spider%202.jfif?alt=media&token=750610b9-e009-4b2b-b6fa-a28b69507e86'],
+        descripcion:'Super Deportivo de Ferrari',
+    },
+
+    {
+        nombre:'Ferrari F8 Tributo',
+        precio:238077,
+        cantidad:0,
+        fotos:['https://firebasestorage.googleapis.com/v0/b/felipe408crtiendatcc.appspot.com/o/ferrari%20f8%20tributo%201.jpg?alt=media&token=d7eefb06-bf6d-4061-a270-11087900f76e','https://firebasestorage.googleapis.com/v0/b/felipe408crtiendatcc.appspot.com/o/ferrari%20f8%20tributo%202.jfif?alt=media&token=f0536b50-b274-43c1-be5e-c84f14fab896'],
+        descripcion:'Super Deportivo de Ferrari',
+    },
+
+    {
+        nombre:'Ferrari FF',
+        precio:58500,
+        cantidad:0,
+        fotos:['https://firebasestorage.googleapis.com/v0/b/felipe408crtiendatcc.appspot.com/o/Ferrari-FF%201.jpg?alt=media&token=a01c326f-6703-4e8f-8ff2-f9eb9f191ebb','https://firebasestorage.googleapis.com/v0/b/felipe408crtiendatcc.appspot.com/o/ferrari-FF%202.webp?alt=media&token=314439ad-2f46-45e5-a1d6-82eace20fffc'],
+        descripcion:'Super Deportivo de Ferrari',
+    },
+
+    {
+        nombre:'Ferrari F12 Berlinetta',
+        precio:310000,
+        cantidad:0,
+        fotos:['https://firebasestorage.googleapis.com/v0/b/felipe408crtiendatcc.appspot.com/o/Ferrari-F12-Berlinetta-1.jpg?alt=media&token=0fca4f66-a2e9-47f1-a731-037ad3fb5e6f','https://firebasestorage.googleapis.com/v0/b/felipe408crtiendatcc.appspot.com/o/ferrari%20F12%20berlinetta%202.jfif?alt=media&token=01641653-8bbb-44d6-83c2-8468a693d403'],
+        descripcion:'Super Deportivo de Ferrari',
+    },
+
+    {
+        nombre:'La Ferrari',
+        precio:1300000,
+        cantidad:0,
+        fotos:['https://firebasestorage.googleapis.com/v0/b/felipe408crtiendatcc.appspot.com/o/la%20ferrari%201.jpg?alt=media&token=afa9a5ee-d3d0-42fd-904c-1c78751d4546','https://firebasestorage.googleapis.com/v0/b/felipe408crtiendatcc.appspot.com/o/la%20ferarri%202.jfif?alt=media&token=71f39198-e7da-47a4-93e7-3f1827703821'],
+        descripcion:'Super Deportivo de Ferrari',
+    },
+
+    {
+        nombre:'Lamborghini Aventador',
+        precio:382210,
+        cantidad:0,
+        fotos:['https://firebasestorage.googleapis.com/v0/b/felipe408crtiendatcc.appspot.com/o/lamborghini%20aventador.jfif?alt=media&token=eb4eda37-1bb0-4d90-a588-4acbc7408ce5','https://firebasestorage.googleapis.com/v0/b/felipe408crtiendatcc.appspot.com/o/lamborghini%20aventador%202.jpg?alt=media&token=d7f63448-578b-4167-9ca0-0c5c10ee5523'],
+        descripcion:'Super Deportivo de Lamborghini',
+    },
+
+    {
+        nombre:'Lamborghini Veneno',
+        precio:3000000,
+        cantidad:0,
+        fotos:['https://firebasestorage.googleapis.com/v0/b/felipe408crtiendatcc.appspot.com/o/lamborghini%20veneno%201.jpg?alt=media&token=24009240-220b-45f1-ad9e-2bf55ff75808','https://firebasestorage.googleapis.com/v0/b/felipe408crtiendatcc.appspot.com/o/lamborghini%20veneno%202.jfif?alt=media&token=d6a6b3b9-df91-4865-b386-a28069e89de8'],
+        descripcion:'Super Deportivo de Lamborghini',
+    },
+
+    {
+        nombre:'Lamborghini Gallardo',
+        precio:160169,
+        cantidad:0,
+        fotos:['https://firebasestorage.googleapis.com/v0/b/felipe408crtiendatcc.appspot.com/o/lamborghini-gallardo-1.jpg?alt=media&token=f5b02da4-c61c-4de7-ab83-ad56fd339e1c','https://firebasestorage.googleapis.com/v0/b/felipe408crtiendatcc.appspot.com/o/lamborghini-gallardo-2.jfif?alt=media&token=88a2153a-9cbb-4953-887d-46f3956fbff2'],
+        descripcion:'Super Deportivo de Lamborghini',
+    },
+
+    {
+        nombre:'Lamborghini Urus',
+        precio:171500,
+        cantidad:0,
+        fotos:['https://firebasestorage.googleapis.com/v0/b/felipe408crtiendatcc.appspot.com/o/lamborghini-urus1.jfif?alt=media&token=48d41557-bbca-45f5-a7e8-a244316cdaca','https://firebasestorage.googleapis.com/v0/b/felipe408crtiendatcc.appspot.com/o/lamborghini-urus%202.jfif?alt=media&token=f3201abf-cca7-4985-8744-89a3377cd98e'],
+        descripcion:'Super Deportivo de Lamborghini',
+    },
+
+    {
+        nombre:'Lamborghini Huracan',
+        precio:230000,
+        cantidad:0,
+        fotos:['https://firebasestorage.googleapis.com/v0/b/felipe408crtiendatcc.appspot.com/o/lamborghini-huracan%201.jfif?alt=media&token=cc9a883d-6679-4c79-8ba1-b62ce7224716','https://firebasestorage.googleapis.com/v0/b/felipe408crtiendatcc.appspot.com/o/lamborghini-huracan%202.webp?alt=media&token=d35b1b14-e028-466a-aa07-690e266d21cc'],
+        descripcion:'Super Deportivo de Lamborghini',
+    },
+    
+    /*
+    {
+        nombre:'',
+        precio:,
+        cantidad:0,
+        fotos:[],
+        descripcion:'',
+    }, */
+
+
+]
+
---
+++ src/controladores/controladorPrincipal.js
@@ -0,0 +1,5 @@
+// Importaciones
+
+import {productosBD} from './controladorDatos.js'; 
+import {} from '';
+
